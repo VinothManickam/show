263c6ff07371e6daf8ec3d4ee10efadd21105463
VINOTH <103953710+VinothManickam@users.noreply.github.com>
2023-08-24 12:02:10 +0530

    Dashboard

--- App.js
+++ App.js
@@ -1,73 +1,75 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { Menu, Button } from 'antd';
-import { HomeOutlined, StarOutlined, MessageOutlined } from '@mui/icons-material';
-import { SettingOutlined } from "@ant-design/icons"; // Import the SettingOutlined icon
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCalendarAlt, faEye } from '@fortawesome/free-solid-svg-icons';
 
-function App() {
-  const [stories, setStories, setBlogData] = useState([
-    { id: 1, title: 'Sample Story 1', summary: 'This is the summary of Sample Story 1.' },
-    { id: 2, title: 'Sample Story 2', summary: 'This is the summary of Sample Story 2.' },
-  ]);
+const Card = ({ imageUrl, title, createdDate, numberOfViews }) => {
+  return (
+    <div className="card">
+      <div className="image-container">
+        <img src={imageUrl} alt={title} />
+      </div>
+      <div className="content">
+        <h2>Blog {title}</h2>
+        <p>
+          <FontAwesomeIcon icon={faCalendarAlt} /> {createdDate}
+        </p>
+        <p>
+          <FontAwesomeIcon icon={faEye} />  {numberOfViews}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+const CardModel = ({ title, lastUpdateDate }) => {
+  const currentDate = new Date();
+  const lastUpdate = new Date(lastUpdateDate);
+  const monthNames = [
+    'January', 'February', 'March', 'April', 'May', 'June',
+    'July', 'August', 'September', 'October', 'November', 'December'
+  ];
+
+  return (
+    <div className="card-model">
+      <h2>{title}</h2>
+      <p>Last Update: {monthNames[lastUpdate.getMonth()]} {lastUpdate.getDate()}</p>
+    </div>
+  );
+};
+
+const App = () => {
+  const [cardData, setCardData] = useState({
+    imageUrl: '',
+    title: '',
+    createdDate: '',
+    numberOfViews: 0,
+  });
 
   useEffect(() => {
-    // Make an API request to your Flask backend here
-    axios.get('http://127.0.0.1:5000/api/posts/company_a')  // Replace 'company_a' with the desired company name
-      .then(response => {
-        setBlogData(response.data);
-      })
-      .catch(error => {
-        console.error('Error fetching data:', error);
-      });
+    // Fetch data from backend API
+    fetch('YOUR_BACKEND_API_URL')
+      .then(response => response.json())
+      .then(data => setCardData(data))
+      .catch(error => console.error('Error fetching data:', error));
   }, []);
 
   return (
-    <div className="app-container">
-      <div className="left-side">
-        <Menu
-          items={[
-            { label: 'Home', icon: <HomeOutlined /> },
-            { label: 'DiaryBlog Admin', icon: <StarOutlined /> },
-            { label: 'Typelt Admin', icon: <MessageOutlined /> },
-            { label: 'Settings', icon: <SettingOutlined /> },
-          ]}
-        ></Menu>
+    <div className="app">
+      <Card
+        imageUrl={cardData.imageUrl}
+        title={cardData.title}
+        createdDate={cardData.createdDate}
+        numberOfViews={cardData.numberOfViews}
+      />
+      <div className="drafts-section">
+        <CardModel
+          title="Inside Title"
+          lastUpdateDate="2023-08-20"
+        />
       </div>
-      <form>
-      <div class="content row"> 
-        <h1>Web Components That Just Works.</h1>
-        <p>Welcome to our online store!</p>  
-      </div>
-      <br />
-      <div class="row">
-      <Button className="create-blog-button">CREATE NEW POST</Button>
-      <h1 className="blog-heading">List Of Post</h1>
-    </div>
-    
-    <table className="story-table">
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Summary</th>
-            <th>View</th>
-          </tr>
-        </thead>
-        <tbody>
-          {stories.map(story => (
-            <tr key={story.id}>
-              <td>{story.title}</td>
-              <td>{story.summary}</td>
-              <td>
-                <button>View</button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-      </form>
     </div>
   );
-}
+};
 
 export default App;
